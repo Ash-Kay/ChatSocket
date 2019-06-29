ae2d22e1b28755fb6c409549eafabeec83b06f09
Ashish Kumar <akuzi770@gmail.com>
2019-06-29 11:54:04 +0530

    room feature

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('user connected')
 
+    socket.on('join-room', function(roomId) {
+        console.log(roomId +" : has been joined by someone"  );
+        io.to(roomId).emit(" someone joined ")
+    })
+
     socket.on('join', function(userNickname) {
         console.log(userNickname +" : has joined the chat "  );
-        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
+        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat server ");
     })
 
     socket.on('messagedetection', (senderNickname,messageContent) => {
